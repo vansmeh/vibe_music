1c19fee19fe3e4a38ea73ba667accdba7644ef39
vansmeh <152426817+vansmeh@users.noreply.github.com>
2026-04-23 18:31:04 +0000

    git push

--- client/src/services/vibeService.js
+++ client/src/services/vibeService.js
@@ -1,9 +1,11 @@
 import { vibeClassifier } from './vibeClassifier';
+import { getStableVibe } from '../utils/stableVibe';
 
 // classifyVibe(features) -> one of: "party_peak", "social", "chill", "background"
 export async function classifyVibe(features) {
   // Expect features: { energy, centroid, zcr }
-  return vibeClassifier(features || {});
+  const classified = vibeClassifier(features || {});
+  return getStableVibe(classified);
 }
 
 export default { classifyVibe };
