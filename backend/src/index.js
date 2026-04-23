4663f4678a6313d98884ce8f01c5f4b0fab29cc1
vansmeh <152426817+vansmeh@users.noreply.github.com>
2026-04-23 19:04:26 +0000

    git push

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-// Start server
+const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
 });
